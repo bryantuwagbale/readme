3c43f77a1fed3fcc1d11ffb3be289bd00a708c57
bryantuwagbale <78828750+bryantuwagbale@users.noreply.github.com>
2021-05-20 21:30:14 -0500

    added array of questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,29 +1,40 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const fetch = require("node-fetch");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        message: 'What is the title of your project?',
-        name: 'title',
-    },
-    {
-        type: 'input',
-        message: 'What is your project about?',
-        name: 'desc',
-    },
-    {
-        type: 'input',
-        message: 'What license do you need?',
-        name: 'license',
-    },
-    {
-        type: 'input',
-        message: 'What is your GitHub username?',
-        name: 'username',
-    },
-];
-
+function promtUser() {
+    return inquirer.prompt(
+         [
+            {
+                type: 'input',
+                message: 'What is the title of your project?',
+                name: 'title',
+            },
+            {
+                type: 'input',
+                message: 'What is your project about?',
+                name: 'desc',
+            },
+            {
+                type: 'input',
+                message: 'What license do you need?',
+                name: 'license',
+            },
+            {
+                type: 'input',
+                message: 'What is your GitHub username?',
+                name: 'username',
+            },
+        ]
+        
+    
+}
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
