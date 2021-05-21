658699643e3d94272537359ce3ecd767dc14857a
bryantuwagbale <78828750+bryantuwagbale@users.noreply.github.com>
2021-05-20 21:16:09 -0500

    basic structure

---
+++ Develop/index.js
@@ -0,0 +1,34 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is your project about?',
+        name: 'desc',
+    },
+    {
+        type: 'input',
+        message: 'What license do you need?',
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'username',
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
